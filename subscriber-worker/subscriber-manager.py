e4f62717c7be7d9797433a93518bcd1005c9541e
soma5722 <89482153+soma5722@users.noreply.github.com>
2021-11-28 11:53:12 -0700

    subscriber manager v 2

--- subscriber-worker/subscriber-manager.py
+++ subscriber-worker/subscriber-manager.py
@@ -14,11 +14,16 @@ import smtplib
 
 hostname = platform.node()
 
-##
-## Configure test vs. production
-##
+##Rabbit MQ commands 
+## CMD SUBCMD ARG1 ARG2 ARG3
+## CMD - 00 Default 01 subscribe, 02 - unsubscribe, 03 0nWeatherChange
+## SUBCMD - 00 Default
+## (ARG1 ARG2 ARG3) - (null null null) Default
+## 
 redisHost = os.getenv("REDIS_HOST") or "localhost"
 rabbitMQHost = os.getenv("RABBITMQ_HOST") or "localhost"
+adminEmailId = 'soma5722@colorado.edu'
+adminEmailPsw = ''
 
 print(f"Connecting to rabbitmq({rabbitMQHost}) and redis({redisHost})")
 
@@ -30,10 +35,14 @@ def sendGmail(receiiverEmailId, senderEmailId, senderEmailPsw, content):
    s.quit()
 
 def subscribe(emailId):
+    subscriberList.append(emailId)
     return
 def unsubscribe(emailId):
+    subscriberList.remove(emailId)
     return
-def onWeatherChange():
+def onWeatherChange(message):
+    for subscriber in subscriberList:
+       sendGmail(subscriber,adminEmailId,adminEmailPsw, message)
     return
 
 
@@ -42,15 +51,34 @@ def callback(ch, method, properties, body):
     
     print(" [x] Received %r" % body.decode())
     string  = body.decode('utf-8')
+    cmd  =  string.split(' ')
+    if(cmd == "01"):
+      if(len(cmd)<3):
+        print(" subscribe cmd is not proper, follow CMD SUBCMD ARG1 ARG2 ARG3")
+      subscribe(cmd[2])
     
+    if(cmd == "02"):
+      if(len(cmd)<3):
+        print(" unsubscribe cmd is not proper, follow CMD SUBCMD ARG1 ARG2 ARG3")
+      subscribe(cmd[2])
+
+    if(cmd == "03"):
+      if(len(cmd)<5):
+        print(" unsubscribe cmd is not proper, follow CMD SUBCMD ARG1 ARG2 ARG3")
+        onWeatherChange(cmd[2]+" "+cmd[3]+" "+cmd[4])
+
+subscriberList = []
+#
+# Add code to read the persistant subscriber list from db
+#
 
 rabbitMQ = pika.BlockingConnection(
         pika.ConnectionParameters(host='rabbitmq'))
 rabbitMQChannel = rabbitMQ.channel()
 
-rabbitMQChannel.queue_declare(queue='toWorker')
+rabbitMQChannel.queue_declare(queue='toSubscriberWorker')
 print(' [*] Waiting for messages. To exit press CTRL+C')
 rabbitMQChannel.basic_qos(prefetch_count=1)
-rabbitMQChannel.basic_consume(queue='toWorker', on_message_callback=callback,auto_ack=True)
+rabbitMQChannel.basic_consume(queue='toSubscriberWorker', on_message_callback=callback,auto_ack=True)
 rabbitMQChannel.start_consuming()
-rabbitMQChannel.exchange_declare(exchange='logs', exchange_type='topic')
+#rabbitMQChannel.exchange_declare(exchange='logs', exchange_type='topic')
