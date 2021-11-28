134e7580744c4ac696d0030db169b4aedf4e5c81
soma5722 <89482153+soma5722@users.noreply.github.com>
2021-11-28 12:19:36 -0700

    deploying file added

---
+++ deploy-all.sh
@@ -0,0 +1,15 @@
+kubectl apply -f redis/redis-deployment.yaml
+kubectl apply -f redis/redis-service.yaml
+
+kubectl apply -f rabbitmq/rabbitmq-deployment.yaml
+kubectl apply -f rabbitmq/rabbitmq-service.yaml
+
+#kubectl apply -f rest/rest-deployment.yaml
+#kubectl apply -f rest/rest-service.yaml
+#kubectl apply -f rest/rest-ingress-gc.yaml
+#kubectl apply -f rest/rest-ingress.yaml
+
+kubectl apply -f logs/logs-deployment.yaml
+
+kubectl apply -f subscriber-worker/subscriber-deployment.yaml
+kubectl apply -f subscriber-worker/subscriber-service.yaml
