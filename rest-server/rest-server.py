ff5255c3c16a538b3e92dd0a116886bef9c0bbd7
soma5722 <89482153+soma5722@users.noreply.github.com>
2021-11-30 15:34:59 -0700

    adding rest endpoiunt codes

--- rest-server/rest-server.py
+++ rest-server/rest-server.py
@@ -14,10 +14,60 @@ import logging
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.DEBUG)
 
+##Rabbit MQ commands 
+## CMD SUBCMD ARG1 ARG2 ARG3
+## CMD - 00 Default 01 subscribe, 02 - unsubscribe, 03 0nWeatherChange, 05 getSpecificRouteWeather
+## SUBCMD - 00 Default
+## (ARG1 ARG2 ARG3) - (null null null) Default
+## 
+
 @app.route('/')
 def root():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello, World new final project!</p>"
 
+@app.route('/api/subscribe/<EmailId>', methods=['GET'])
+def subscribe(EmailId):
+    rabbitMQ = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+    rabbitMQChannel = rabbitMQ.channel()
+    rabbitMQChannel.queue_declare(queue='toComputeEngine')
+    print("got a request to subscribe for the api from user with email id " + EmailId)
+    message = '01'+' '+ EmailId
+    rabbitMQChannel.basic_publish(exchange='',routing_key='toComputeEngine', body=message)
+    rabbitMQChannel.close()
+    rabbitMQ.close()
+    result = {"action":"queued"}
+    response_pickled = jsonpickle.encode(result)
+    return Response(response=response_pickled, status=200, mimetype="application/json")
+
+@app.route('/api/unsubscribe/<EmailId>', methods=['GET'])
+def unsubscribe(EmailId):
+    rabbitMQ = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+    rabbitMQChannel = rabbitMQ.channel()
+    rabbitMQChannel.queue_declare(queue='toComputeEngine')
+    print("got a request to subscribe for the api from user with email id " + EmailId)
+    message = '02'+' '+ EmailId
+    rabbitMQChannel.basic_publish(exchange='',routing_key='toComputeEngine', body=message)
+    rabbitMQChannel.close()
+    rabbitMQ.close()
+    result = {"action":"queued"}
+    response_pickled = jsonpickle.encode(result)
+    return Response(response=response_pickled, status=200, mimetype="application/json")
+
+
+
+@app.route('/api/getSpecificRouteWeather/<startLoc>/<endLoc>', methods=['GET'])
+def getSpecificRouteWeather(startLoc, endLoc):
+    rabbitMQ = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+    rabbitMQChannel = rabbitMQ.channel()
+    rabbitMQChannel.queue_declare(queue='toComputeEngine')
+    print("got a request to getSpecificRouteWeather from user with startLoc " + startLoc + " endLoc "+endLoc)
+    message = '05'+' '+ startLoc+' '+endLoc
+    rabbitMQChannel.basic_publish(exchange='',routing_key='toComputeEngine', body=message)
+    rabbitMQChannel.close()
+    rabbitMQ.close()
+    result = {"action":"queued"}
+    response_pickled = jsonpickle.encode(result)
+    return Response(response=response_pickled, status=200, mimetype="application/json")
 
 
 # start flask app
